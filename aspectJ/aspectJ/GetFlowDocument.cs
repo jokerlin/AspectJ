1bc907251799cf4b7a20b2f83eb81daa96785dbf
gaigai <greferry@gmail.com>
2014-05-19 19:44:20 +0800

    新增GetFlowDocument类 调用getFlowDocument(path)函数可以得到Document对象

---
+++ aspectJ/aspectJ/GetFlowDocument.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+
+namespace aspectJ
+{
+    class GetFlowDocument
+    {
+        public static FlowDocument getFlowDocument(string path)
+        {
+            FlowDocument flowDocument = new FlowDocument();
+            StreamReader srReadFile = new StreamReader(path);
+
+            while (!srReadFile.EndOfStream)
+            {
+                string strReadLine = srReadFile.ReadLine();
+                var p = new Paragraph();
+                var r = new Run(strReadLine);
+                p.Inlines.Add(r);
+                flowDocument.Blocks.Add(p);
+            }
+            srReadFile.Close();
+            return flowDocument;
+        }
+    }
+}
